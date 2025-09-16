8e5cbea15e6c0a9b9428c53997a3c78e9481add4
Tamir Yamaleyev <tamirka8@gmail.com>
2025-09-17 01:00:15 +0300

    backups for merge

---
+++ Assets/Scripts/PlayerControllerBackup.cs
@@ -0,0 +1,167 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public LifeController lifeUI;
+    public enum Weapon
+    {
+        Laser,
+        PlasmaCannon,
+        Lightning
+    }
+
+    public Weapon currentWeapon = Weapon.Laser;
+    public int maxAmmo = 5;
+    public int plasmaAmmo = 0;
+    public int lightningAmmo = 0;
+    public HUDWeapon hudWeapon;
+
+    public GameObject firePoint;
+    public GameObject laserProjectile;
+    public GameObject plasmaProjectile;
+    public GameObject lightningProjectile;
+
+    public int lives = 3;
+
+    public float moveSpeed = 5f;
+    public float bulletSpeed = 10f;
+    public float bulletDestroyDelay = 3f;
+    private Rigidbody2D rb;
+
+    private float halfPlayerWidth;
+    private float horizontalLimit;
+
+    public Quaternion bulletRotation = Quaternion.Euler(0f, 0f, 90f);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lifeUI = FindFirstObjectByType<LifeController>();
+
+        rb = GetComponent<Rigidbody2D>();
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        halfPlayerWidth = sr.bounds.extents.x;
+
+        Camera cam = Camera.main;
+        float screenHalfWidth = cam.orthographicSize * cam.aspect;
+
+        horizontalLimit = screenHalfWidth - halfPlayerWidth;
+
+        hudWeapon = FindFirstObjectByType<HUDWeapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        ClampPosition();
+        ShootingHandler();
+    }
+
+    void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontal * moveSpeed, 0);
+    }
+
+    void ClampPosition()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -horizontalLimit, horizontalLimit);
+        transform.position = pos;
+    }
+
+    void ShootingHandler()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (currentWeapon)
+            {
+                case Weapon.Laser:
+                    ShootLaser();
+                    break;
+                case Weapon.PlasmaCannon:
+                    ShootPlasma();
+                    plasmaAmmo--;
+                    hudWeapon.SetAmmo(Weapon.PlasmaCannon, plasmaAmmo);
+
+                    if (plasmaAmmo <= 0) currentWeapon = Weapon.Laser;
+                    break;
+                case Weapon.Lightning:
+                    ShootLightning();
+                    lightningAmmo--;
+                    hudWeapon.SetAmmo(Weapon.Lightning, lightningAmmo);
+
+                    if (lightningAmmo <= 0) currentWeapon = Weapon.Laser;
+                    break;
+            }
+            hudWeapon.ChangeWeapon(currentWeapon);
+        }
+    }
+
+    public void PickUpWeaponBox(Weapon type)
+    {
+        hudWeapon.ChangeWeapon(type);
+        hudWeapon.SetAmmo(type, maxAmmo);
+
+        switch (type)
+        {
+            case Weapon.PlasmaCannon:
+                plasmaAmmo = maxAmmo;
+                currentWeapon = Weapon.PlasmaCannon;
+                break;
+            case Weapon.Lightning:
+                lightningAmmo = maxAmmo;
+                currentWeapon = Weapon.Lightning;
+                break;
+            default: currentWeapon = Weapon.Laser;
+                break;
+        }
+    }
+
+    void ShootLaser()
+    {
+        GameObject bullet = Instantiate(laserProjectile, firePoint.transform.position, bulletRotation);
+        bullet.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, bulletSpeed);
+        Destroy(bullet, bulletDestroyDelay);
+    }
+
+    void ShootPlasma()
+    {
+        GameObject bullet = Instantiate(plasmaProjectile, firePoint.transform.position, bulletRotation);
+        bullet.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, bulletSpeed);
+        Destroy(bullet, bulletDestroyDelay);
+    }
+
+    void ShootLightning()
+    {
+        gameObject.GetComponent<LightningWeapon>().FireLightning();
+    }
+
+    public void TakeDamage()
+    {
+        lives--;
+        lifeUI.UpdateLives(lives);
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over!");
+        Time.timeScale = 0f;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("EnemyProjectile"))
+        {
+            Destroy(other.gameObject);
+            TakeDamage();
+        }
+    }
+}
